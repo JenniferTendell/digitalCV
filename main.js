ab444dab9cc9165d70c0f1da9a67e220e13c7734
Jennifer Tendell <jennifer.tendell@gmail.com>
2020-11-09 17:23:01 +0100

    Make Parallax scrolling effect on header.

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ window.addEventListener('load', main);
 let previousPosition = 0;
 
 function main() {
-    window.onscroll = toggleMenu;
+    window.addEventListener('scroll', toggleMenu);
+    moveHeaderImages();
 }
 
 /* Show menu on scroll up, hide on scroll down */
@@ -17,3 +18,18 @@ function toggleMenu() {
 
     previousPosition = window.scrollY;
 }
+
+/* Move header images on scroll */
+function moveHeaderImages() {
+    const headerMountains = document.getElementById('header-layer-2');
+    const headerGothenburg = document.getElementById('header-layer-3');
+
+    window.addEventListener('scroll', function() {
+        let value = window.scrollY;
+
+        headerMountains.style.top = value * .05 + 'rem';
+        headerGothenburg.style.top = value * .02 + 'rem';      
+    })  
+}
+
+
